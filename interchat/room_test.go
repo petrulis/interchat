8d476bc0c86d0e8de01f58733746c51ce38bff10
Justinas Petrulis <justinas.petrulis@gmail.com>
2021-07-18 17:23:36 +0300

    Test if message gets broadcasted to all connected clients

--- interchat/room_test.go
+++ interchat/room_test.go
@@ -59,16 +59,40 @@ func TestRoom(t *testing.T) {
 		assert.Equal(tt, 0, len(r.clients))
 	})
 
-	t.Run("client receives message", func(tt *testing.T) {
+	t.Run("check all clients receives a message", func(tt *testing.T) {
 		r := newRoom("public", nil)
 		go r.open()
 
-		client1 := &client{}
-		client2 := &client{}
+		numOfClients := 10000
+		clients := make([]*client, numOfClients)
+		for i := 0; i < numOfClients; i++ {
+			clients[i] = &client{
+				send: make(chan []byte),
+			}
+		}
+		for _, client := range clients {
+			r.join <- client
+		}
 
-		r.join <- client1
-		r.join <- client2
+		wg := sync.WaitGroup{}
+		wg.Add(numOfClients)
 
+		ch := make(chan []byte, numOfClients)
+		after := time.After(1 * time.Second)
+
+		read := func(c *client) {
+			defer wg.Done()
+			select {
+			case <-after:
+				return
+			case ch <- <-c.send:
+			}
+		}
+		for _, client := range clients {
+			go read(client)
+		}
 		r.forward <- []byte("message")
+		wg.Wait()
+		assert.Equal(t, numOfClients, len(ch))
 	})
 }
